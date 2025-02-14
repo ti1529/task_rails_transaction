0301bd52cea0ce2caa2aa8477c36b7c7fec5f486
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-02-14 19:57:40 +0900

    fix:

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,13 +11,11 @@ class OrdersController < ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      @order = current_user.orders.lock.build(order_params)
-      unless @order.save
-        raise ActiveRecord::Rollback
-      end
+      @order = current_user.orders.build(order_params)
+      @order.save
       @order.update_total_quantity
+      # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     end
-    # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     redirect_to orders_path
   end
 
