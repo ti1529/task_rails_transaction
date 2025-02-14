257378c7e124455f6d9be91bf231ef64ec3d6ae9
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-02-14 17:03:33 +0900

    fix: 悲観的ロックの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,9 +10,13 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = current_user.orders.build(order_params)
-    @order.save
-    @order.update_total_quantity
+    ActiveRecord::Base.transsaction do
+      @order = current_user.orders.lock.build(order_params)
+      unless @order.save
+        raise ActiveRecord::Rollback
+      end
+      @order.update_total_quantity
+    end
     # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     redirect_to orders_path
   end
